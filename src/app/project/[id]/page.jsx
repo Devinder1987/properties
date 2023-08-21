c25446f36f6053d95899d12e70b25dbf0171bac1
Devinder1987 <d.shutwal@gmail.com>
2023-08-21 22:02:00 +0530

    Filters

--- src/app/project/[id]/page.jsx
+++ src/app/project/[id]/page.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
 import ProjectDetails from './ProjectDetails';
-import projectData from '../../projectData';
+import { projectData } from '../../projectData';
 
 export default function ProductDetailsPage({ params }) {
   const project = projectData.find((val) => val.id == params.id);
--- src/app/projectList/[category]/page.jsx
+++ src/app/projectList/[category]/page.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Layout from '../../../components/Layout';
 import ProjectTiles from './ProjectTiles';
-import projectData from '../../projectData';
+import { projectData } from '../../projectData';
 
 export default function ProductListsPage({ params }) {
   return (
